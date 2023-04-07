3be4db858a191b3f38e5c6427abefb1f841844ac
Ghost <mobicom.dg@gmail.com>
2023-04-07 23:43:37 +0300

    фронт
    с багом

--- TrafficLights/Form1.Designer.cs
+++ TrafficLights/Form1.Designer.cs
@@ -20,20 +20,19 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
 
         /// <summary>
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
+        public Form1()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Game X-0";
+            this.Load += new System.EventHandler(this.Form1_Load);
         }
 
-        #endregion
     }
 }
